a6cdf1cd6a3ee01bca8c814f5c2e6173a70c2af7
filip-codes <filip@bitecorn.com>
2025-09-11 14:36:19 +0200

    feat: added String.after() method

--- lib/string.ex
+++ lib/string.ex
@@ -1,5 +1,33 @@
 defmodule Support.String do
   @doc """
+  Return the remainder of a string after the first occurrence of a given value.
+
+  ## Examples
+
+      iex> alias Support.String
+      Support.String
+
+      iex> String.after("Elixir Package", "Elixir ")
+      "Package"
+  """
+  @doc since: "0.2.0"
+  def unquote(:after)(value, search)
+      when is_binary(value) and (is_binary(search) or is_integer(search)) do
+    search_string = to_string(search)
+
+    cond do
+      search_string == "" ->
+        value
+
+      true ->
+        case String.split(value, search_string, parts: 2) do
+          [_, part_after] -> part_after
+          [_] -> value
+        end
+    end
+  end
+
+  @doc """
   Reverse the given string.
 
   ## Examples
--- test/string_test.exs
+++ test/string_test.exs
@@ -1,8 +1,20 @@
 alias Support.String
 
 defmodule StringTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
   doctest Support.String
+
+  test "after" do
+    assert "nah" == String.after("hannah", "han")
+    assert "nah" == String.after("hannah", "n")
+    assert "nah" == String.after("ééé hannah", "han")
+    assert "hannah" == String.after("hannah", "xxxx")
+    assert "hannah" == String.after("hannah", "")
+    assert "nah" == String.after("han0nah", "0")
+    assert "nah" == String.after("han0nah", 0)
+    assert "nah" == String.after("han2nah", 2)
+  end
 
   test "reverse" do
     assert "FooBar" == String.reverse("raBooF")
